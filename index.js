c0f25d303f6f9ee6da78bf9caffd99feffa19c5c
s111ew <167631312+s111ew@users.noreply.github.com>
2024-09-23 19:35:05 +0100

    Add HTML skeleton

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv'
 import * as utils from 'utils.js'
+import { formatDate } from './utils'
 dotenv.config()
 
 const API_KEY = process.env.API_KEY
@@ -18,17 +19,23 @@ const getWeather = async (location) => {
 
 const dataObjectFrom = (data) => {
   return {
-    place: data.resolvedAddress,
+    place: utils.formatPlace(data.resolvedAddress),
     temp: utils.farenheitToCelcius(data.days[0].temp),
     tempMin: utils.farenheitToCelcius(data.days[0].tempmin),
     tempMax: utils.farenheitToCelcius(data.days[0].tempmax),
     feelsLike: utils.farenheitToCelcius(data.days[0].feelslike),
-    date: data.days[0].datetime,
+    date: utils.formatDate(data.days[0].datetime),
     weatherDesc: data.days[0].conditions,
-    precipChance: data.days[0].precipprob,
-    windSpeed: data.days[0].windspeed,
+    precipChance: `${data.days[0].precipprob}%`,
+    windSpeed: `${data.days[0].windspeed}km/h`,
     windDirec: data.days[0].winddir,
   }
+}
+
+const searchBox = {
+  input: document.querySelector('input'),
+  button: document.querySelector('button'),
+
 }
 
 const renderData = (data) => {
