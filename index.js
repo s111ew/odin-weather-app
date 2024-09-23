989768cb0e0da68872b3ddf459189f622b84ea8b
s111ew <167631312+s111ew@users.noreply.github.com>
2024-09-23 16:14:04 +0100

    Add logic for initial 'get weather' async function

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Hello via Bun!");
+import dotenv from 'dotenv'
+dotenv.config()
+
+const API_KEY = process.env.API_KEY
+
+const getWeather = async (location) => {
+  const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/?key=${API_KEY}`
+  try {
+    const response = await fetch(url, {mode: 'cors'})
+    const data = await response.json()
+    console.log(data)
+  } catch (error) {
+    console.error('Error:', error)
+  }
+}
+
+getWeather('London')
+
