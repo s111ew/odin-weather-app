395ee1894211d98da709891e7271010a9c3dbf23
s111ew <167631312+s111ew@users.noreply.github.com>
2024-09-23 17:37:13 +0100

    add css style reset

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import dotenv from 'dotenv'
+import * as utils from 'utils.js'
 dotenv.config()
 
 const API_KEY = process.env.API_KEY
@@ -7,12 +8,33 @@ const getWeather = async (location) => {
   const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/?key=${API_KEY}`
   try {
     const response = await fetch(url, {mode: 'cors'})
-    const data = await response.json()
-    console.log(data)
+    const weatherData = await response.json()
+    const dataObject = dataObjectFrom(weatherData);
+    renderData(dataObject)
   } catch (error) {
     console.error('Error:', error)
   }
 }
 
-getWeather('London')
+const dataObjectFrom = (data) => {
+  return {
+    place: data.resolvedAddress,
+    temp: utils.farenheitToCelcius(data.days[0].temp),
+    tempMin: utils.farenheitToCelcius(data.days[0].tempmin),
+    tempMax: utils.farenheitToCelcius(data.days[0].tempmax),
+    feelsLike: utils.farenheitToCelcius(data.days[0].feelslike),
+    date: data.days[0].datetime,
+    weatherDesc: data.days[0].conditions,
+    precipChance: data.days[0].precipprob,
+    windSpeed: data.days[0].windspeed,
+    windDirec: data.days[0].winddir,
+  }
+}
+
+const renderData = (data) => {
+
+}
+
+
+getWeather('')
 
---
+++ utils.js
@@ -0,0 +1,3 @@
+export const farenheitToCelcius = (temp) => {
+  return ((temp - 32) * (5 / 9)).toFixed(1)
+}
